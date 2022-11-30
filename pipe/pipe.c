bb3dc81db1262fa6f9015085ef523552536ea385
Abhinavxox <abhinavxox@gmail.com>
2022-11-30 16:26:19 +0530

    <>:[]

---
+++ pipe/pipe.c
@@ -0,0 +1,26 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+int main()
+{
+    int fd[2], n;
+    char buffer[100];
+    pid_t p;
+    pipe(fd);
+    p = fork();
+    if(p>0)
+    {
+        printf("Passing value to child \n");
+        write(fd[1], "hello\n", 6);
+    }
+    
+    else
+    {
+        printf("Received data in child \n");
+        n = read(fd[0], buffer, 100);
+        write(1,buffer,n);
+
+   }
+    
+}
